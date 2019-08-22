27765654ca15885e57f44a409297cd2befab9b98
David De Marco <david.demarco@demarcogaming.com>
2019-08-22 09:27:39 -0700

    Initial load of files to website :wq

---
+++ Assets/Scripts/Problem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MathsOperation
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    Exponent,
+    Fraction
+}
+
+[System.Serializable]
+public class Problem
+{
+    // problem: 5 + 10
+    // answers: 20, 8, 15, 30
+    // correct tube: 2 
+
+    public string firstNumber;
+    public string secondNumber;
+    public MathsOperation operation;
+    public string[] answers;
+
+    public int correctTube;
+}
